8eb399c30d7f03b601caac42980787b5de6b6528
Ryan Harris <harrisryan1@gmail.com>
2020-08-15 15:04:11 -0400

    add code for useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 
-import WeatherDisplay from "./02-use-effect/WeatherDisplay";
-import { fetchDataFromAPI, parseDataForDay } from "./02-use-effect/utils";
+import Circles from "./03-use-layout-effect/Circles";
 
 function App() {
-  const [day, setDay] = useState("today");
-  const [data, setData] = useState(null);
-  const [dayData, setDayData] = useState(null);
-
-  useEffect(() => {
-    document.title = `${new Date().toLocaleString()} on Mars`;
-  });
-
-  useEffect(() => {
-    const apiData = fetchDataFromAPI();
-
-    apiData
-      .then((res) => setData(res))
-      .catch((err) => {
-        console.log(err);
-        setData(null);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (data) {
-      const dataForDay = parseDataForDay(data);
-      setDayData(data[dataForDay]);
-    }
-  }, [day, data]);
-
   return (
     <div className="App">
       <header className="App__header">
-        <p>useEffect</p>
+        <p>useLayoutEffect</p>
       </header>
-      <main className="App__content">
-        <WeatherDisplay day={day} setDay={setDay} dayData={dayData} />
+      <main className="App__main">
+        <Circles />
       </main>
     </div>
   );
